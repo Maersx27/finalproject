ee32d841564b03f85cb92f954ec092539c09417b
unknown <chloroville.27@gmail.com>
2023-03-13 17:28:36 +0700

    Install Firebase untuk login google

--- lib/view/main/profile/profile_page.dart
+++ lib/view/main/profile/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:finalpro/constants/r.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -11,10 +12,176 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        title: Text("Akun Saya"),
+        centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "Edit",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
       body: Column(
         children: [
-          Text("Profile"),
+          Container(
+            padding: EdgeInsets.only(top: 20, bottom: 60, left: 15, right: 15),
+            decoration: BoxDecoration(
+                color: R.colors.primary,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(9),
+                  bottomRight: Radius.circular(9),
+                )),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Nama User",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400),
+                      ),
+                      Text(
+                        "Sekolah User",
+                        style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  R.assets.icAvatar,
+                  width: 50,
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(blurRadius: 7, color: Colors.black.withOpacity(0.25)),
+              ],
+            ),
+            padding: EdgeInsets.symmetric(vertical: 18, horizontal: 13),
+            margin: EdgeInsets.symmetric(vertical: 18, horizontal: 13),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Identitas Diri"),
+                SizedBox(height: 15),
+                Text(
+                  "Nama Lengkap",
+                  style: TextStyle(
+                    color: R.colors.greySubtitleHome,
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  "Nama Lengkap User",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Email",
+                  style: TextStyle(
+                    color: R.colors.greySubtitleHome,
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  "user@gmail.com",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Jenis Kelamin",
+                  style: TextStyle(
+                    color: R.colors.greySubtitleHome,
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  "Not Identify",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                  ),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  "Kelas",
+                  style: TextStyle(
+                    color: R.colors.greySubtitleHome,
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  "Human",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 13,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 13),
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(blurRadius: 7, color: Colors.black.withOpacity(0.25)),
+              ],
+            ),
+            // child: ListTile(
+            //   title: Text(
+            //     "Keluar",
+            //     style: TextStyle(color: Colors.red),
+            //   ),
+            //   leading: Icon(
+            //     Icons.exit_to_app,
+            //     color: Colors.red,
+            //   ),
+            // ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.exit_to_app,
+                  color: Colors.red,
+                ),
+                SizedBox(width: 10),
+                Text(
+                  "Keluar",
+                  style: TextStyle(color: Colors.red),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
